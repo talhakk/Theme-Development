181538359cafc44e9ea678f681bbd15bdaca42e8
Talha Khan <talhakhanandco@gmail.com>
2023-12-30 13:29:15 +0500

    ACF required

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 /**
  * Register block styles.
  */
+require_once get_template_directory().'/class-tgm-plugin-activation.php';
 
 if ( ! function_exists( 'talha_block_styles' ) ) :
 	/**
@@ -138,40 +139,6 @@ endif;
 add_action( 'init', 'talha_block_styles' );
 
 /**
- * Enqueue block stylesheets.
- */
-
-if ( ! function_exists( 'talha_block_stylesheets' ) ) :
-	/**
-	 * Enqueue custom block stylesheets
-	 *
-	 * @since Twenty Twenty-Four 1.0
-	 * @return void
-	 */
-	function talha_block_stylesheets() {
-		/**
-		 * The wp_enqueue_block_style() function allows us to enqueue a stylesheet
-		 * for a specific block. These will only get loaded when the block is rendered
-		 * (both in the editor and on the front end), improving performance
-		 * and reducing the amount of data requested by visitors.
-		 *
-		 * See https://make.wordpress.org/core/2021/12/15/using-multiple-stylesheets-per-block/ for more info.
-		 */
-		wp_enqueue_block_style(
-			'core/button',
-			array(
-				'handle' => 'talha-button-style-outline',
-				'src'    => get_parent_theme_file_uri( 'assets/css/button-outline.css' ),
-				'ver'    => wp_get_theme( get_template() )->get( 'Version' ),
-				'path'   => get_parent_theme_file_path( 'assets/css/button-outline.css' ),
-			)
-		);
-	}
-endif;
-
-add_action( 'init', 'talha_block_stylesheets' );
-
-/**
  * Register pattern categories.
  */
 
@@ -195,3 +162,76 @@ if ( ! function_exists( 'talha_pattern_categories' ) ) :
 endif;
 
 add_action( 'init', 'talha_pattern_categories' );
+
+
+/**
+ * TGM ACF Plugin Activation Class
+ *
+ */
+
+ add_action( 'tgmpa_register', 'talha_register_required_plugins' );
+
+ // This function is called from the above hook
+ function talha_register_required_plugins(){
+     // The plugins array allows us to define multiple plugins we want to include.
+     // The commented out example shows how we can include and activation a bundled
+     // plugin zip file in our theme.
+     $plugins = array(
+        /*		array(
+                'name'               => 'Advanced Custom Fields',
+                'slug'               => 'advanced-custom-fields',
+                'source'             => get_stylesheet_directory() . '/theme/plugins/advanced-custom-fields.zip',
+                'required'           => true,
+                'version'            => '',
+                'force_activation'   => true, // Force activation because we need advanced custom fields,
+                'force_deactivation' => false,
+                'external_url'       => ''
+            ),
+        */
+            // This below definition will include the ACF plugin from the Wordpress.org plugins repository
+            // and if permissions permit, will allow for it to be automatically downloaded and installed.
+            // If permissions don't allow, the user will be prompted into downloading the plugin themselves
+            // and installing it manually.
+            array(
+                'name' 		=> 'Advanced Custom Fields',
+                'slug' 		=> 'advanced-custom-fields',
+                'required' 	=> true,
+            ),
+        );
+    
+        $theme_text_domain = 'talha';
+    
+        $config = array(
+            'domain'       		=> $theme_text_domain,
+            'default_path' 		=> '',
+            'parent_menu_slug' 	=> 'themes.php',
+            'parent_url_slug' 	=> 'themes.php',
+            'menu'         		=> 'install-required-plugins',
+            'has_notices'      	=> true,
+            'is_automatic'    	=> false,
+            'message' 			=> '',
+            'strings'      		=> array(
+                'page_title'                       			=> __( 'Install Required Plugins', $theme_text_domain ),
+                'menu_title'                       			=> __( 'Install Plugins', $theme_text_domain ),
+                'installing'                       			=> __( 'Installing Plugin: %s', $theme_text_domain ), // %1$s = plugin name
+                'oops'                             			=> __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+                'notice_can_install_required'     			=> _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
+                'notice_can_install_recommended'			=> _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
+                'notice_cannot_install'  					=> _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
+                'notice_can_activate_required'    			=> _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+                'notice_can_activate_recommended'			=> _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+                'notice_cannot_activate' 					=> _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s)
+                'notice_ask_to_update' 						=> _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s)
+                'notice_cannot_update' 						=> _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
+                'install_link' 					  			=> _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+                'activate_link' 				  			=> _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+                'return'                           			=> __( 'Return to Required Plugins Installer', $theme_text_domain ),
+                'plugin_activated'                 			=> __( 'Plugin activated successfully.', $theme_text_domain ),
+                'complete' 									=> __( 'All plugins installed and activated successfully. %s', $theme_text_domain ), // %1$s = dashboard link
+                'nag_type'									=> 'updated' // Determines admin notice type - can only be 'updated' or 'error'
+            )
+        );
+    
+        tgmpa( $plugins, $config );
+    
+}
